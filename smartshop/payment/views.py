3b8e306608e6c539fb7357af3803b1ebc6e200f8
Eduardo Rojas <carlos.edhr@protonmail.com>
2023-03-03 12:18:33 -0800

    Included order items to Stripe checkout session in 'payment_process'. Included payment_completed and payment_canceled views. Added payment URLs.

--- smartshop/payment/views.py
+++ smartshop/payment/views.py
@@ -9,7 +9,11 @@ from orders.models import Order
 stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
 
+
 def payment_process(request):
+    '''
+    Function to handle the payment process
+    '''
     order_id = request.session.get('order_id', None)
     order = get_object_or_404(Order, id=order_id)
 
@@ -24,9 +28,27 @@ def payment_process(request):
             'cancel_url': cancel_url,
             'line_items': []
         }
+        # add order items to the Stripe checkout session
+        for item in order.items.all():
+            session_data['line_items'].append({
+                'price_data': {
+                'unit_amount': int(item.price * Decimal('100')),
+                'currency': 'usd', 
+                'product_data':{
+                'name': item.product_name,
+                },
+                },
+                'quantity': item.quantity,
+            })
         # create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
         # redirect to Stripe payment form
         return redirect(session.url, code=303)
     else:
         return render(request, 'payment/process.html', locals())
+
+def payment_completed(request):
+    return render(request, 'payment/completed.html')
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
