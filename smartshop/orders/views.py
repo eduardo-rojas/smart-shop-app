264cdc12af6e6ddbe9f9c64b79edf1adbbddec06
Eduardo Rojas <carlos.edhr@protonmail.com>
2023-03-05 22:38:57 -0800

    Added 'stripe_id' field to 'Order' model, this model now takes a unique Stripe payment identifier each time a purchase is successful. Stripe integration completed.

--- smartshop/orders/views.py
+++ smartshop/orders/views.py
@@ -6,28 +6,26 @@ from cart.cart import Cart
 from .tasks import order_created
 
 def order_create(request):
-    cart = Cart(request) 
+    cart = Cart(request)
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order=order,
-                                         product=item['product'],
-                                         price=item['price'],
-                                         quantity=item['quantity'])
+                                        product=item['product'],
+                                        price=item['price'],
+                                        quantity=item['quantity'])
+            # clear the cart
             cart.clear()
-            #Asynchronous email notification
+            # launch asynchronous task
             order_created.delay(order.id)
-            #set the order in session
-            request.session['order_id'] = order.id 
-            #redirect for payment 
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
             return redirect(reverse('payment:process'))
-        # return render(request,
-        #             'orders/order/created.html',
-        #             {'order': order})
     else:
         form = OrderCreateForm()
-    return render(request, 
+    return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
